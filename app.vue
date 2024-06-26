04202e8af757c74305bda9d032d12d3770f7ebb9
muratkiziloz <muratkiziloz@bilfen.com>
2024-06-26 15:11:04 +0300

    table sorting added

--- app.vue
+++ app.vue
@@ -1,31 +1,36 @@
+<template>
+  <MyTable
+      :headers="headers"
+      :rows="rows"
+      headerClass="my-custom-header-class"
+      :headerStyle="{ backgroundColor: '#ecebeb', color: '#333' }"
+      sortAscIcon="sort-asc"
+      sortDescIcon="sort-desc"
+      sortDefaultIcon="icon"
+  />
+</template>
+
 <script setup lang="ts">
 import { ref } from 'vue';
 
 const headers = ref([
   { text: 'Name', value: 'name', searchValue: 'name', operator: 'equals' },
   { text: 'Age', value: 'age', searchValue: 'age', operator: 'equals' },
+  { text: 'Agesdas', value: 'age2', searchValue: 'age', operator: 'equals' },
+  { text: 'Age35rf', value: 'age4', searchValue: 'age', operator: 'equals' },
 ]);
 
 const rows = ref([
-  { name: 'John Doe', age: 30 },
-  { name: 'Jane Smith', age: 25 },
+  { name: 'John Doe', age: 30, age2: 'tesf', age4: 'test' },
+  { name: 'Jane Smith', age: 25, age2: 'tesf', age4: 'test2'  },
+  { name: 'Jane Smith', age: 25, age2: 'tesf', age4: 'test2'  },
+  { name: 'Jane Smith', age: 25, age2: 'tesf', age4: 'test2'  },
+  { name: 'Jane Smith', age: 25, age2: 'tesf', age4: 'test2'  },
+  { name: 'Jane Smith', age: 25, age2: 'tesf', age4: 'test2'  },
+  { name: 'Jane Smith', age: 25, age2: 'tesf', age4: 'test2'  },
+  { name: 'Jane Smith', age: 25, age2: 'tesf', age4: 'test2'  },
+  { name: 'Jane Smith', age: 25, age2: 'tesf', age4: 'test2'  },
 ]);
-
 </script>
 
-<template>
-  <MyTable
-      :headers="headers"
-      :rows="rows"
-      headerClass="my-custom-header-class"
-      :headerStyle="{ backgroundColor: 'blue', color: 'white' }"
-  />
-</template>
-
-<style lang="css" scoped>
-.my-custom-header-class {
-  font-size: 40px;
-  text-align: center;
-  color: black;
-}
-</style>
+<style scoped></style>
--- components/MyTable.vue
+++ components/MyTable.vue
@@ -3,60 +3,150 @@
     <table>
       <thead :class="headerClass" :style="headerStyle">
       <tr>
-        <th v-for="(header, index) in headers" :key="index">{{ header.text }}</th>
+        <th v-for="header in headers" :key="header.value" @click="sortTable(header.value)">
+          {{ header.text }}
+          <span :class="getSortIcon(header.value)"></span>
+        </th>
       </tr>
       </thead>
       <tbody>
-      <tr v-for="(row, rowIndex) in rows" :key="rowIndex">
-        <td v-for="(header, index) in headers" :key="index">{{ row[header.value] }}</td>
+      <tr v-for="(row, rowIndex) in sortedRows" :key="rowIndex">
+        <td v-for="header in headers" :key="header.value">{{ row[header.value] }}</td>
       </tr>
       </tbody>
     </table>
   </div>
 </template>
 
-<script setup>
-import { defineProps } from 'vue';
+<script setup lang="ts">
+import { ref, computed } from 'vue';
 
 const props = defineProps({
   headers: {
     type: Array,
-    required: true,
-    validator: (value) => {
-      return value.every(item =>
-          typeof item.text === 'string' &&
-          typeof item.value === 'string' &&
-          typeof item.searchValue === 'string' &&
-          typeof item.operator === 'string'
-      );
-    }
+    required: true
   },
   rows: {
     type: Array,
-    required: true,
-    validator: (value) => {
-      return value.every(item => typeof item === 'object');
-    }
+    required: true
   },
   headerClass: {
     type: String,
-    default: ''
+    required: false
   },
   headerStyle: {
     type: Object,
-    default: () => ({})
+    required: false
+  },
+  sortAscIcon: {
+    type: String,
+    required: true
+  },
+  sortDescIcon: {
+    type: String,
+    required: true
+  },
+  sortDefaultIcon: {
+    type: String,
+    required: true
   }
 });
+
+const sortKey = ref('');
+const sortOrder = ref(1);
+
+const sortedRows = computed(() => {
+  if (!sortKey.value) {
+    return props.rows;
+  }
+  return [...props.rows].sort((a, b) => {
+    if (a[sortKey.value] < b[sortKey.value]) {
+      return -1 * sortOrder.value;
+    } else if (a[sortKey.value] > b[sortKey.value]) {
+      return 1 * sortOrder.value;
+    }
+    return 0;
+  });
+});
+
+const sortTable = (key: string) => {
+  if (sortKey.value === key) {
+    sortOrder.value = -sortOrder.value;
+  } else {
+    sortKey.value = key;
+    sortOrder.value = 1;
+  }
+};
+
+const getSortIcon = (key: string) => {
+  if (sortKey.value === key) {
+    return sortOrder.value === 1 ? props.sortAscIcon : props.sortDescIcon;
+  }
+  return props.sortDefaultIcon;
+};
 </script>
 
 <style scoped>
 table {
   width: 100%;
   border-collapse: collapse;
+  background-color: #f9f9f9;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
 }
+
 th, td {
   border: 1px solid #ddd;
-  padding: 8px;
+  padding: 12px 15px;
+  text-align: left;
 }
+
+th {
+  cursor: pointer;
+  position: relative;
+  //background-color: #ffffff;
+  //color: #333;
+  font-weight: 600;
+  text-transform: uppercase;
+  transition: background-color 0.3s ease, color 0.3s ease;
+}
+
+th:hover {
+  background-color: #f1f1f1;
+}
+
+tbody tr {
+  transition: background-color 0.3s ease;
+}
+
+tbody tr:nth-child(even) {
+  background-color: #f7f7f7;
+}
+
+tbody tr:hover {
+  background-color: #ececec;
+}
+
+.icon {
+  display: inline-block;
+  margin-left: 8px;
+  font-size: 0.2em;
+}
+
+.sort-asc::after {
+  content: '▲';
+  font-size: 0.2em;
+}
+
+.sort-desc::after {
+  content: '▼';
+  font-size: 0.2em;
+}
+
+.icon::after {
+  content: '⇅';
+  font-size: 0.2em;
+}
+
 
 </style>
